c286e1f5b391e1ba8769b51be0566e311fa7b9e3
Nickgubso6 <165740437+Nickgubso6@users.noreply.github.com>
2024-04-02 02:44:34 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,11 +2,11 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
-global.ytname = "YT: AyushBotz.inc"
-global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.ownernumber = ['09048097390']
+global.ownername = "クラトス"//owner name
+global.ytname = "baqeeolanrewaju"
+global.socialm = "GitHub: Nickgubs06"
+global.location = "Nigeria"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
